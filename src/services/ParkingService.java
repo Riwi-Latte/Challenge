aae018343ce449eb3d2ed61ba9341a78955478b3
isabellaPulgarin <isabellapul7@gmail.com>
2025-09-17 19:35:12 -0500

    fix: create the ArrayList the parked vehicles and vehicles record

--- src/services/ParkingService.java
+++ src/services/ParkingService.java
@@ -1,11 +1,14 @@
 // ParkingService.java
 package services;
 
-import javax.   swing.JOptionPane;
+import java.util.*;
+import javax.swing.JOptionPane;
 import models.Vehicle;
 
 public class ParkingService {
 
+    List<Vehicle> parkedVehicles = new ArrayList<>();
+    List<Vehicle> vehiclesRecord = new ArrayList<>();
     public Vehicle registerEntry() {
         String licensePlate = JOptionPane.showInputDialog("Enter the vehicle license plate:");
         String type = JOptionPane.showInputDialog("Enter the vehicle type (MOTORCYCLE, CAR, SUV):");
@@ -14,19 +17,31 @@ public class ParkingService {
 
         Vehicle vehicle = new Vehicle(licensePlate, type, level, entryTime);
         
+        parkedVehicles.add(vehicle);
+        vehiclesRecord.add(vehicle);
+
         // Show vehicle details for confirmation
-        JOptionPane.showMessageDialog(null, "Vehicle registered:\n" +
-                                            "License Plate: " + vehicle.getLicensePlate() + "\n" +
-                                            "Type: " + vehicle.getType() + "\n" +
-                                            "Level: " + vehicle.getLevel() + "\n" +
-                                            "Entry Time: " + entryTime);
+        JOptionPane.showMessageDialog(null, parkedVehicles);
         return vehicle;
     }
 
-    // public void registerExit(ParkingTicket ticket) {
-    //     String exitTime = JOptionPane.showInputDialog("Enter the exit time (HH:mm):");
-    //     ticket.setExitTime(exitTime);
-        
-    //     // Logic for calculating fee and displaying receipt can be added here
-    // }
+    public List<Vehicle> getParkedVehicles() {
+        return parkedVehicles;
+    }
+
+    public List<Vehicle> getVehiclesRecord() {
+        return vehiclesRecord;
+    }
+
+     public void registerExit() {
+
+        String licensePlate = JOptionPane.showInputDialog("Enter the vehicle license plate:");
+        String exitTime = JOptionPane.showInputDialog("Enter the exit time (HH:mm):");
+        String agreement = JOptionPane.showInputDialog("Enter convenio if applicable, otherwise leave blank:");
+
+
+         
+  
+         // Logic for calculating fee and displaying receipt can be added here
+     }
 }
